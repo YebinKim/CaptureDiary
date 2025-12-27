21584f515cef73ec7316abe86defeb199010a98c
YebinKim <yb2316@naver.com>
2025-12-27 21:32:38 +0900

    init: create a project files

---
+++ CaptureDiary/CaptureDiary/CaptureDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureDiaryApp.swift
+//  CaptureDiary
+//
+//  Created by vivi on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CaptureDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaptureDiary/CaptureDiary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CaptureDiary
+//
+//  Created by vivi on 12/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("캡처일기")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
